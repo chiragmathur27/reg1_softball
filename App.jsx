6219db17ffddaee277cb31ab0a9039152b8bf8b3
Milind-Srivastava2002 <125573324+Milind-Srivastava2002@users.noreply.github.com>
2023-02-19 15:56:40 +0530

    Update App.jsx
    Updated: "I have previous experience in Softball."

--- App.jsx
+++ App.jsx
@@ -40,7 +40,7 @@ const sIGNUPBASEBALL32Data = {
     emailAddress2: "Email Address",
     mobileNumber: "Mobile Number",
     pinCodeOnlyAvailableForBaltimore: "Pin Code (only available for Baltimore)",
-    iHavePreviousExperienceInBaseball: "I have previous experience in Baseball.",
+    iHavePreviousExperienceInBaseball: "I have previous experience in Softball.",
     spanText1: "I agree to the ",
     spanText2: "Terms of Service",
     spanText3: " ",
